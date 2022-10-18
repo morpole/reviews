6cd2301aa0d189015a2efd6fdd5f527407bdc14f
morpole <morpole@gmail.com>
2022-10-18 18:07:17 +0100

    added remainder of logic

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// local reviews data
+ // local reviews data
 const reviews = [
   {
     id: 1,
@@ -9,7 +9,7 @@ const reviews = [
     text:
       "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   },
-  {
+  { 
     id: 2,
     name: "anna johnson",
     job: "web designer",
@@ -37,3 +37,60 @@ const reviews = [
       "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
+
+// select items
+
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
+
+// set starting item
+
+let currentItem = 0;
+
+// load initial item
+
+window.addEventListener('DOMContentLoaded', function(){
+ showPerson()
+});
+
+// show person based on item
+
+function showPerson(){
+ const item = reviews[currentItem];
+ img.src = item.img;
+ author.textContent = item.name;
+ job.textContent = item.job;
+ info.textContent = item.text;
+}
+
+// show next person
+
+nextBtn.addEventListener('click', function(){
+  currentItem++;
+  if(currentItem > reviews.length -1){
+    currentItem = 0;
+  }
+  showPerson();
+});
+
+// show previous person
+
+prevBtn.addEventListener('click', function() {
+  currentItem--;
+  if(currentItem < 0){
+    currentItem = reviews.length - 1;
+  }
+  showPerson();
+})
+
+// show random person
+randomBtn.addEventListener('click', function() {
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson();
+});
